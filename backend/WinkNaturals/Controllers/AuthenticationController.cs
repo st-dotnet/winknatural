fa3f6de5a6a35e885560d3d660d18be27e8b3348
st-karanjamwal <karanjamwal@gmail.com>
2021-07-16 17:28:40 +0530

    Initial commit "backend"

---
+++ backend/WinkNaturals/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WinkNaturals.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        #region Create Customer
+
+        [HttpPost]
+        public IActionResult CreateCustomer()
+        {
+            return null;
+        }
+
+        #endregion
+    }
+}
