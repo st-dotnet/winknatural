9e0064c5722e8bbadfb635719c7cefac980b145e
St Dev <stdev.net@gmail.com>
2021-07-28 21:36:52 +0530

    Worked on the product detail page.

--- frontend/src/app/shop/product-detail/product-detail.component.ts
+++ frontend/src/app/shop/product-detail/product-detail.component.ts
@@ -7,6 +7,8 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  incredShow : Boolean= false;
+
   customOptions: OwlOptions = {
     loop: true,
     mouseDrag: false,
@@ -31,10 +33,64 @@ export class ProductDetailComponent implements OnInit {
     },
     nav: true
   }
+  slide_reviews: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: true
+  }
+
+  product_reviews: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 4
+      },
+      940: {
+        items: 6
+      }
+    },
+    nav: true
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleShow() {
+
+    this.incredShow = ! this.incredShow;
+    
+    }
 
 }
