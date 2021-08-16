14b3cbf78cff557a24bd4fe6e19e7e294e63f74e
Karan Bir <karanjamwal@gmail.com>
2021-08-16 21:24:07 +0530

    Worked on checkout and thank you page

--- frontend/src/app/components/store/store.module.ts
+++ frontend/src/app/components/store/store.module.ts
@@ -9,13 +9,15 @@ import { ShopComponent } from './shop/shop.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
 
 @NgModule({
   declarations: [
     ShopComponent,
     ProductDetailComponent,
     CartComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    ThankYouComponent
   ],
   imports: [
     SharedModule,
