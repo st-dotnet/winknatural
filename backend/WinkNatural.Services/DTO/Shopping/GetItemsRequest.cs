b84ad18325d842b2e927b5cc27b6be2822c0186e
Karan Bir <karanjamwal@gmail.com>
2021-08-16 19:22:00 +0530

    Fix small issue

---
+++ backend/WinkNatural.Services/DTO/Shopping/GetItemsRequest.cs
@@ -0,0 +1,24 @@
+﻿namespace WinkNatural.Services.DTO.Shopping
+{
+    public class GetItemsRequest
+    { 
+        public GetItemsRequest()
+        {
+            this.ItemCodes = new string[0];
+            this.IncludeDynamicKitChildren = true;
+        } 
+        // public IOrderConfiguration Configuration { get; set; }
+        public int? CategoryID { get; set; }
+        public string[] ItemCodes { get; set; }
+        public bool IncludeChildCategories { get; set; }
+        public int PriceTypeID { get; set; }
+        public int LanguageID { get; set; }
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+        public int SortBy { get; set; }
+        public bool IncludeDynamicKitChildren { get; set; }
+        public bool IncludeLongDescriptions { get; set; }
+        public bool IgnoreCache { get; set; }
+        public object Configuration { get; internal set; }
+    }
+}
